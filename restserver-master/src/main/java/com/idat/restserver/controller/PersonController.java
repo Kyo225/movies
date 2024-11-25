a2ecafab4a8248ae7fbb7b63a08c6350933e4700
kyo225 <sm40627133@idat.pe>
2024-11-25 11:02:38 -0500

    aqui

---
+++ restserver-master/src/main/java/com/idat/restserver/controller/PersonController.java
@@ -0,0 +1,116 @@
+package com.idat.restserver.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.idat.restserver.entity.Person;
+
+import com.idat.restserver.repository.PersonRepository;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Path("/person")
+public class PersonController {
+    @Autowired
+    // private static List<Person> persons = new ArrayList<>();
+    private PersonRepository personRepository;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /*static {
+        persons.add(new Person("Juan", 20));
+        persons.add(new Person("Pedro", 30));
+    }*/
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPerson() {
+        try {
+            List<Person> persons = personRepository.findAll();
+            String json = objectMapper.writeValueAsString(persons);
+            return Response.ok(json, MediaType.APPLICATION_JSON).build();
+        } catch (JsonProcessingException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al convertir a JSON")
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updatePerson(@PathParam("id") Long id, String json) {
+        try {
+            Person updatePerson = objectMapper.readValue(json, Person.class);
+            Person person = personRepository.findById(id).orElse(null);
+            if (person != null) {
+                person.setName(updatePerson.getName());
+                person.setAge(updatePerson.getAge());
+                personRepository.save(person);
+                return Response.ok(person, MediaType.APPLICATION_JSON).build();
+            }
+            return Response.status(Response.Status.NOT_FOUND).entity("Persona no encontrada").build();
+        } catch (JsonProcessingException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al procesar la solicitud")
+                    .build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletePerson(@PathParam("id") Long id) {
+        Person person = personRepository.findById(id).orElse(null);
+        if (person != null) {
+            personRepository.delete(person);
+            return Response.ok(person, MediaType.APPLICATION_JSON).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("Persona no encontrada")
+                .type(MediaType.TEXT_PLAIN)
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addPerson(String json) {
+        try {
+            Person newPerson = objectMapper.readValue(json, Person.class);
+            personRepository.save(newPerson);
+            String createdJson = objectMapper.writeValueAsString(newPerson);
+            return Response.status(Response.Status.CREATED)
+                    .entity(createdJson)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        } catch (JsonProcessingException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al procesar la solicitud")
+                    .build();
+        }
+
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonByName(@PathParam("name") String name) {
+        Person person = personRepository.findByName(name); // Busca la persona por nombre
+
+        if (person != null) {
+            return Response.ok(person, MediaType.APPLICATION_JSON).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Persona no encontrada con el nombre: " + name)
+                    .build();
+        }
+    }
+
+}
